0271f9b772752aaf4d6ba5904f4e307d05b63787
nazirumar <nazirumar888@gmail.com>
2021-09-16 23:43:24 +0100

    first commit2

---
+++ Quaz_App/script.js
@@ -0,0 +1,43 @@
+const quezData = [
+    {
+        question: ' How old are you',
+        a_1: '10',
+        1_2: '17',
+        a_3:'20',
+        a_4:'200',
+        correct: 'c'
+    },
+{
+    question: ' what is your fav programming languege',
+    a: 'Java',
+    b: 'python',
+    c: 'c',
+    d: 'javascript',
+    correct: 'b'
+
+},
+{
+    question: ' who is the President of nigeria in 2020',
+    a: 'GoodLuck',
+    b: 'Babangida',
+    c: 'Obasanjo',
+    d: 'yar adua '
+},
+{
+    question: 'what does the HTML stand for?',
+    a: 'hypertext makup languege',
+    b: 'cascadian style sheet',
+    c: 'json object notation',
+    d: 'helicopters terminal nazbeen',
+    correct: 'a'
+
+},{
+    question: 'What year javascript launch',
+    a:'2020',
+    b: '1995',
+    c: '2018',
+    d: 'none of the above',
+    correct: 'd'
+}
+
+]

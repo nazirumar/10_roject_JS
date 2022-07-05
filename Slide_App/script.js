ba2c9bcda5c5a8522167eb9f81e6bd01290a7bfb
nazirumar <nazirumar888@gmail.com>
2022-07-05 21:14:20 +0100

    main

--- Slide_App/script.js
+++ Slide_App/script.js
@@ -1,48 +1,68 @@
-const btnforwadDev = document.querySelector('.forward')
-const btnbackforwadDev = document.querySelector('.forward')
-const imgDev = document.querySelector('.imglist')
+const nextBtn = document.querySelector('.next')
+const prevewBtn = document.querySelector('.prev')
+const img = document.getElementById('person-img')
 
 
 
-const imgeList=[
-    './img/img (1).jpg',
-    './img/img (2).jpg',
-    './img/img (3).jpg',
-    './img/img (4).jpg',
-    './img/img (5).jpg',
-    './img/img (6).jpg',
-    './img/img (7).jpg',
-    './img/img (8).jpg',
-    './img/img (9).jpg',
-    './img/img (10).jpg',
+const  reviews=[
+  {
+    img:'./img/img (1).jpg'
+   },{
+    img:'./img/img (2).jpg'
+   },{
+    img:'./img/img (3).jpg'
+   },{
+    img:'./img/img (4).jpg'
+   },{
+    img:'./img/img (5).jpg'
+   },{
+    img:'./img/img (6).jpg'
+   },{
+    img:'./img/img (7).jpg'
+   },
+   {
+    img:'./img/img (8).jpg'
+   },
+   {
+    img:'./img/img (9).jpg'
+   },
+   {
+    img:'./img/img (10).jpg'
+   },
 
-]
+];
 
-let slideIndex = 1;
-showSlides(slideIndex);
+let currentItem = 0;
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+
+window.addEventListener("DOMContentLoaded", function () {
+  ShowImage(currentItem);
+})
+
+function ShowImage(image) {
+  const item = reviews[image]
+  img.src = item.img;
+  
+
 }
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
 
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("demo");
-  let captionText = document.getElementById("caption");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+nextBtn.addEventListener('click', () => {
+  currentItem++;
+  if (currentItem > reviews.length -1){
+    currentItem = 0;
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+    ShowImage(currentItem)
+
+})
+
+
+
+prevewBtn.addEventListener('click', () => {
+  currentItem++;
+  if (currentItem > reviews.length - 1){
+    currentItem = 0;
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
-}
+    ShowImage(currentItem)
+
+})
